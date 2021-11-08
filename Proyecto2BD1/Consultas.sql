d56f9fab853a8844636e9ffe6ec279f5085cd631
GersonBelteton <55422983+GersonBelteton@users.noreply.github.com>
2021-11-08 07:47:09 -0600

    FINISHED

--- Proyecto2BD1/Consultas.sql
+++ Proyecto2BD1/Consultas.sql
@@ -25,6 +25,62 @@ where nombre_sexo = 'mujeres'
 group by nombre_pais, nombre_departamento
 order by nombre_pais;
 
+-- 3
+select sc6.nombre_pais, sc7.partido, sc6.maxalcaldia from
+(select sc5.nombre_pais, max(sc5.conteoalcaldia)as maxalcaldia from
+(select sc4.nombre_pais, sc4.partido, count(sc4.partido)as conteoalcaldia from
+(select sc1.nombre_pais, sc1.partido, sc1.nombre_municipio from 
+(select pais.nombre_pais, municipio.nombre_municipio, partido.partido, sum(alfabetos+analfabetos+primaria+nivel_medio+universitarios)as totalvotos
+from pais inner join region on pais.id_pais = region.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio
+inner join eleccionpartido on conteovoto.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+group by pais.nombre_pais, municipio.nombre_municipio, partido.partido)as sc1
+,
+(select sc3.nombre_pais, sc3.nombre_municipio, max(totalvotos)as maxvotos
+from (select pais.nombre_pais, municipio.nombre_municipio, partido.partido, sum(alfabetos+analfabetos+primaria+nivel_medio+universitarios)as totalvotos
+from pais inner join region on pais.id_pais = region.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio
+inner join eleccionpartido on conteovoto.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+group by pais.nombre_pais, municipio.nombre_municipio, partido.partido)as sc3
+group by sc3.nombre_pais, sc3.nombre_municipio
+)as sc2
+where  sc1.nombre_municipio = sc2.nombre_municipio
+and sc1.totalvotos = sc2.maxvotos) as sc4
+group by sc4.nombre_pais, sc4.partido)as sc5
+group by sc5.nombre_pais)as sc6,
+(select sc4.nombre_pais, sc4.partido, count(sc4.partido)as conteoalcaldia from
+(select sc1.nombre_pais, sc1.partido, sc1.nombre_municipio from 
+(select pais.nombre_pais, municipio.nombre_municipio, partido.partido, sum(alfabetos+analfabetos+primaria+nivel_medio+universitarios)as totalvotos
+from pais inner join region on pais.id_pais = region.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio
+inner join eleccionpartido on conteovoto.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+group by pais.nombre_pais, municipio.nombre_municipio, partido.partido)as sc1
+,
+(select sc3.nombre_pais, sc3.nombre_municipio, max(totalvotos)as maxvotos
+from (select pais.nombre_pais, municipio.nombre_municipio, partido.partido, sum(alfabetos+analfabetos+primaria+nivel_medio+universitarios)as totalvotos
+from pais inner join region on pais.id_pais = region.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio
+inner join eleccionpartido on conteovoto.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+group by pais.nombre_pais, municipio.nombre_municipio, partido.partido)as sc3
+group by sc3.nombre_pais, sc3.nombre_municipio
+)as sc2
+where  sc1.nombre_municipio = sc2.nombre_municipio
+and sc1.totalvotos = sc2.maxvotos) as sc4
+group by sc4.nombre_pais, sc4.partido)as sc7
+where sc6.nombre_pais = sc7.nombre_pais
+and sc6.maxalcaldia = sc7.conteoalcaldia;
 
 -- 4
 
@@ -55,6 +111,14 @@ select n.total,n.nombre_region, n.nombre_pais from
 )as a
 group by  nombre_pais,nombre_region)as m
 where n.total = m.total and n.nombre_raza = 'INDIGENAS';
+-- 5
+select pais.nombre_pais, departamento.nombre_departamento, municipio.nombre_municipio, cantvotos from
+
+select nombre_municipio, un from(
+select municipio.nombre_municipio, sum(universitarios)as un, sum(nivel_medio)as nm, sum(primaria)as pr from municipio 
+inner join conteovoto on conteovoto.id_Municipio = municipio.id_municipio
+group by nombre_municipio)as sc1
+where un > (0.25*pr) and un < (0.3*nm);
 
 -- 6
 
@@ -135,6 +199,20 @@ order by nombre_pais, nombre_raza) as a
 group by nombre_pais)as m
 where m.nombre_pais = n.nombre_pais;
 
+-- 10
+
+select sc1.nombre_pais, (max(sc1.cantidadvotos)-min(sc1.cantidadvotos))as diferencia from (
+select nombre_pais, partido, sum(alfabetos+analfabetos)as cantidadvotos from pais
+inner join region on region.id_pais = pais.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio
+inner join eleccionpartido on eleccionpartido.id_eleccionpartido = conteovoto.id_eleccionpartido
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+group by nombre_pais, partido) as sc1
+group by sc1.nombre_pais
+order by diferencia limit 1;
+
 -- 11
 
 select sum(alfabetos)as total, nombre_sexo, nombre_raza from conteovoto
@@ -142,3 +220,34 @@ inner join sexo on conteovoto.id_sexo = sexo.id_sexo
 inner join raza on conteovoto.id_raza = raza.id_raza
 where nombre_sexo = 'mujeres' and nombre_raza = 'INDIGENAS'
 group by nombre_sexo, nombre_raza;
+
+-- 12
+
+select nombre_pais, round((sum(analfabetos)*100)/(sum(analfabetos+alfabetos)),3)as porcentaje from pais
+inner join region on region.id_pais = pais.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio 
+group by nombre_pais
+order by  porcentaje desc limit 1;
+
+-- 13
+select sc1.nombre_departamento, sc1.votos from (
+select nombre_departamento, sum(alfabetos+analfabetos)as votos from pais
+inner join region on region.id_pais = pais.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio 
+where pais.nombre_pais = "Guatemala"
+group by nombre_departamento
+) as sc1,
+(
+select sum(alfabetos+analfabetos)as votosGuate from pais
+inner join region on region.id_pais = pais.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+inner join conteovoto on conteovoto.id_municipio = municipio.id_municipio 
+where pais.nombre_pais = "Guatemala"
+and departamento.nombre_departamento = "Guatemala"
+)as sc2
+where sc1.votos > sc2.votosGuate;
--- Proyecto2BD1/p2.sql
+++ Proyecto2BD1/p2.sql
@@ -12,15 +12,27 @@ drop table region;
 drop table pais;
 drop table temporal;
 
+select count(*) from conteovoto;
+select count(*) from sexo;
+select count(*) from raza;
+select count(*) from eleccionpartido;
+select count(*) from eleccion;
+select count(*) from partido;
+select count(*) from municipio;
+select count(*) from departamento;
+select count(*) from region;
+select count(*) from pais;
+select count(*) from temporal;
+
 create table temporal(
     nombre_eleccion varchar(30) not null,
     anio_eleccion integer not null,
-    pais varchar(30) not null,
-    region varchar(30) not null,
-    depto varchar(30) not null,
-    municipio varchar(30) not null,
+    pais varchar(50) not null,
+    region varchar(50) not null,
+    depto varchar(50) not null,
+    municipio varchar(50) not null,
     partido varchar(30)not null,
-    nombre_partido varchar(30) not null,
+    nombre_partido varchar(50) not null,
     sexo varchar(16) not null,
     raza varchar(16) not null,
     analfabetos integer not null,
@@ -85,7 +97,7 @@ create table departamento(
 
 create table municipio(
     id_municipio integer auto_increment primary key,
-    nombre_municipio varchar(30),
+    nombre_municipio varchar(50),
     id_departamento integer,
     constraint fk_departamento
     foreign key(id_departamento)
@@ -130,6 +142,8 @@ create table conteovoto(
 );
 select count(*) from temporal;
 
+
+
 -- ----------------------------------------------------------carga de datos
 
 
@@ -141,19 +155,49 @@ insert into departamento(nombre_departamento, id_region)select distinct depto, i
 
 insert into municipio(nombre_municipio, id_departamento)select distinct municipio, id_departamento from temporal, departamento, region, pais where departamento.nombre_departamento = temporal.depto and region.nombre_region = temporal.region and pais.nombre_pais = temporal.pais and departamento.id_region = region.id_region;
 
-insert into sexo(nombre_sexo) select distinct sexo from temporal;
+ALTER TABLE temporal ADD id_municipio integer;
+update temporal, municipio, departamento, region, pais set temporal.id_municipio = municipio.id_municipio
+where temporal.municipio = municipio.nombre_municipio
+and temporal.depto = departamento.nombre_departamento
+and temporal.region = region.nombre_region
+and temporal.pais = pais.nombre_pais
+and municipio.id_departamento = departamento.id_departamento
+and departamento.id_region = region.id_region
+and region.id_pais = pais.id_pais;
 
+ALTER TABLE temporal
+ADD id_sexo integer;
+insert into sexo(nombre_sexo) select distinct sexo from temporal;
+update temporal, sexo set temporal.id_sexo = sexo.id_sexo where temporal.sexo = sexo.nombre_sexo;
+
+ALTER TABLE temporal
+ADD  id_raza integer;
 insert into raza(nombre_raza) select distinct raza from temporal;
+update temporal, raza set temporal.id_raza = raza.id_raza where temporal.raza = raza.nombre_raza;
+
+
 
 insert into eleccion(nombre_eleccion, anio) select distinct nombre_eleccion, anio_eleccion from temporal;
 
 insert into partido(partido, nombre_partido) select distinct partido, nombre_partido from temporal;
 
+
+
+ALTER TABLE temporal ADD id_eleccionpartido integer;
 insert into eleccionpartido(id_eleccion, id_partido) select distinct id_eleccion, id_partido from eleccion,partido;
+update temporal, eleccion, partido, eleccionpartido set temporal.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+where temporal.partido = partido.partido
+and temporal.anio_eleccion = eleccion.anio
+and partido.id_partido = eleccionpartido.id_partido
+and eleccion.id_eleccion = eleccionpartido.id_eleccion;
+
+insert into conteovoto(alfabetos,analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido)
+select  distinct alfabetos, analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido 
+from temporal;
 
 
 insert into conteovoto(alfabetos,analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido)
-select  alfabetos, analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido 
+select   alfabetos, analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido 
 from temporal, raza, sexo, municipio, eleccionpartido, eleccion, partido, departamento, region, pais
 where temporal.raza = raza.nombre_raza 
 and temporal.sexo = sexo.nombre_sexo 
@@ -169,5 +213,227 @@ and eleccion.id_eleccion = eleccionpartido.id_eleccion
 and temporal.partido = partido.partido 
 and partido.id_partido = eleccionpartido.id_partido; 
 
+
+select   alfabetos, analfabetos, primaria, nivel_medio, universitarios, id_raza, id_sexo, id_municipio, id_eleccionpartido 
+from temporal inner join raza on temporal.raza = raza.nombre_raza
+inner join sexo on temporal.sexo = sexo.nombre_sexo
+inner join municipio on temporal.municipio = municipio.nombre_municipio
+inner join departamento on municipio.id_departamento = departamento.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join eleccion on temporal.anio_eleccion = eleccion.anio
+inner join partido on temporal.partido = partido.partido
+inner join eleccionpartido on eleccionpartido.id_eleccion = eleccion.id_eleccion
+
+insert into conteovoto(alfabetos,analfabetos, primaria, nivel_medio, universitarios)
+select  distinct alfabetos, analfabetos, primaria, nivel_medio, universitarios
+from temporal;
+
+update conteovoto set id_raza = 
+(select id_raza from raza, temporal 
+where temporal.raza = raza.nombre_raza 
+and temporal.alfabetos = conteovoto.alfabetos
+and temporal.analfabetos = conteovoto.analfabetos
+and temporal.primaria = conteovoto.primaria
+and temporal.nivel_medio = conteovoto.nivel_medio
+and temporal.universitarios = conteovoto.universitarios);
+
+select distinct partido.partido, eleccion.anio, id_eleccionpartido from eleccion, partido, eleccionpartido
+where eleccionpartido.id_eleccion = eleccion.id_eleccion
+and eleccionpartido.id_partido = partido.id_partido;
+
+select distinct pais.nombre_pais, region.nombre_region, departamento.nombre_departamento, municipio.nombre_municipio
+from departamento, region, pais, municipio 
+where municipio.id_departamento = departamento.id_departamento
+and departamento.id_region = region.id_region
+and pais.id_pais = region.id_pais;
+Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.
+
+select  distinct alfabetos, analfabetos, primaria, nivel_medio, universitarios
+from temporal, raza, sexo,
+(select distinct pais.nombre_pais, region.nombre_region, departamento.nombre_departamento, municipio.nombre_municipio
+from departamento, region, pais, municipio 
+where municipio.id_departamento = departamento.id_departamento
+and departamento.id_region = region.id_region
+and pais.id_pais = region.id_pais) ubicacion,
+(select distinct partido.partido, eleccion.anio, id_eleccionpartido from eleccion, partido, eleccionpartido
+where eleccionpartido.id_eleccion = eleccion.id_eleccion
+and eleccionpartido.id_partido = partido.id_partido)ep
+where temporal.raza = raza.nombre_raza
+and temporal.sexo = sexo.nombre_sexo
+and temporal.region = ubicacion.nombre_region
+and temporal.depto = ubicacion.nombre_departamento
+and temporal.municipio = ubicacion.nombre_municipio
+and temporal.anio_eleccion = ep.anio
+and temporal.partido = ep.partido;
+-- ---------------------------CONSULTAS ------------------------------------------------------
+
+-- 1
+select a.nombre_eleccion, a.anio, a.nombre_pais, max(a.total),sum(a.total),max(a.total)*100/sum(a.total)as porcentaje
+from (select  eleccion.nombre_eleccion, eleccion.anio, nombre_pais, partido,sum(alfabetos+analfabetos)as total
+from  conteovoto inner join eleccionpartido  on conteovoto.id_eleccionpartido = eleccionpartido.id_eleccionpartido
+inner join eleccion on eleccion.id_eleccion = eleccionpartido.id_eleccion
+inner join partido on partido.id_partido = eleccionpartido.id_partido
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+group by nombre_pais, partido, nombre_eleccion,anio) as a
+group by nombre_pais,nombre_eleccion,anio;
+
+-- 2
+
+select nombre_pais, nombre_departamento, sum(alfabetos+analfabetos)as total from conteovoto
+inner join municipio on municipio.id_municipio = conteovoto.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join sexo on conteovoto.id_sexo = sexo.id_sexo
+where nombre_sexo = 'mujeres'
+group by nombre_pais, nombre_departamento
+order by nombre_pais;
+
+
+-- 4
+
+select n.total,n.nombre_region, n.nombre_pais from
+(
+    select sum(analfabetos+alfabetos)as total, nombre_raza, nombre_region, nombre_pais from conteovoto
+    inner join raza on conteovoto.id_raza = raza.id_raza
+    inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+    inner join departamento on departamento.id_departamento = municipio.id_departamento
+    inner join region on region.id_region = departamento.id_region
+    inner join pais on pais.id_pais = region.id_pais
+    group by nombre_raza,nombre_region,nombre_pais
+    order by nombre_pais, nombre_raza, nombre_region
+
+)as n
+,
+(select max(a.total)as total,a.nombre_region, a.nombre_pais from
+(
+    select sum(analfabetos+alfabetos)as total, max(alfabetos+analfabetos) as maximo, nombre_raza as nr1, nombre_region, nombre_pais from conteovoto
+    inner join raza on conteovoto.id_raza = raza.id_raza
+    inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+    inner join departamento on departamento.id_departamento = municipio.id_departamento
+    inner join region on region.id_region = departamento.id_region
+    inner join pais on pais.id_pais = region.id_pais
+    group by nombre_raza,nombre_region,nombre_pais
+    order by nombre_pais, nombre_raza, nombre_region
+
+)as a
+group by  nombre_pais,nombre_region)as m
+where n.total = m.total and n.nombre_raza = 'INDIGENAS';
+
+-- 6
+
+select round((mujeres.total/tot.total*100),2)as mujeres, round((tot.total-mujeres.total)/tot.total*100,2)as hombres, mujeres.nombre_departamento from
+(select n.total, n.nombre_departamento, n.nombre_sexo from
+(select sum(universitarios)as total, sexo.nombre_sexo, departamento.nombre_departamento 
+from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join sexo on sexo.id_sexo = conteovoto.id_sexo
+group by sexo.nombre_sexo, departamento.nombre_departamento)as n,
+(select max(a.total) as total,a.nombre_departamento from(
+select sum(universitarios)as total, sexo.nombre_sexo, departamento.nombre_departamento from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join sexo on sexo.id_sexo = conteovoto.id_sexo
+group by sexo.nombre_sexo, departamento.nombre_departamento
+) as a
+group by nombre_departamento)m
+where n.total = m.total and n.nombre_sexo = 'mujeres' 
+order by nombre_departamento) mujeres,
+(select sum(universitarios)as total, departamento.nombre_departamento 
+from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join sexo on sexo.id_sexo = conteovoto.id_sexo
+group by departamento.nombre_departamento)as tot
+where mujeres.nombre_departamento = tot.nombre_departamento;
+
+
+-- 7
+
+select nombre_pais, nombre_region, nombre_departamento, round(avg(alfabetos+analfabetos),2)as promedio from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+group by nombre_pais, nombre_region,nombre_departamento;
+
+-- 8
+
+select nombre_pais, sum(primaria) as primaria, sum(nivel_medio)as nivel_medio, sum(universitarios) as universidad
+from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+group by nombre_pais;
+
+-- 9
+
+select round((n.total/m.total*100),2)as porcentaje, n.nombre_pais,n.nombre_raza from
+(select nombre_pais, nombre_raza, sum(alfabetos+analfabetos)as total
+from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join raza on raza.id_raza = conteovoto.id_raza
+group by nombre_pais, nombre_raza
+order by nombre_pais, nombre_raza)as n,
+(select sum(a.total)as total, a.nombre_pais from
+(select nombre_pais, nombre_raza, sum(alfabetos+analfabetos)as total
+from conteovoto
+inner join municipio on conteovoto.id_municipio = municipio.id_municipio
+inner join departamento on departamento.id_departamento = municipio.id_departamento
+inner join region on region.id_region = departamento.id_region
+inner join pais on pais.id_pais = region.id_pais
+inner join raza on raza.id_raza = conteovoto.id_raza
+group by nombre_pais, nombre_raza
+order by nombre_pais, nombre_raza) as a
+group by nombre_pais)as m
+where m.nombre_pais = n.nombre_pais;
+
+-- 11
+
+select sum(alfabetos)as total, nombre_sexo, nombre_raza from conteovoto
+inner join sexo on conteovoto.id_sexo = sexo.id_sexo
+inner join raza on conteovoto.id_raza = raza.id_raza
+where nombre_sexo = 'mujeres' and nombre_raza = 'INDIGENAS'
+group by nombre_sexo, nombre_raza;
+
+
+-- --------------------------------------------------------------------------------------------------
+
+select + MAX_EXECUTION_TIME(1000)  from conteovoto;
 select * from conteovoto;
 select * from pais;
+select * from raza;
+select * from sexo;
+select * from temporal;
+select * from municipio;
+select * from region;
+select * from partido;
+show open tables where in_use>0;
+
+select distinct  municipio from temporal where pais = "Costa Rica";
+select distinct nombre_municipio from pais 
+inner join  region on region.id_pais = pais.id_pais
+inner join departamento on departamento.id_region = region.id_region
+inner join municipio on municipio.id_departamento = departamento.id_departamento
+where pais.nombre_pais = "Costa Rica";
+show processlist;
+kill 74;
+SHOW ENGINE INNODB STATUS;
+
+set innodb_lock_wait_timeout=100;
+show variables like 'innodb_lock_wait_timeout';
